f2e23fd7765cda6174d219c10262eba2fec2a17a
LiaqatSaeed <liaqatsaeed007@gmail.com>
2018-09-15 23:40:06 +0500

    New UI Changes Made for Portfolio

--- src/App.js
+++ src/App.js
@@ -5,14 +5,64 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+<<<<<<< HEAD
+                 <div class="ct" id="t1">
+        <div class="ct" id="t2">
+          <div class="ct" id="t3">
+            <div class="ct" id="t4">
+            <div class="ct" id="t5">
+            <ul id="menu">
+                <a href="#t1"><li class="icon fa fa-home" id="uno"></li></a>
+                <a href="#t2"><li class="icon fa fa-paint-brush" id="dos"></li></a>
+                <a href="#t3"><li class="icon fa fa-code" id="tres"></li></a>
+                <a href="#t4"><li class="icon fa fa-briefcase" id="cuatro"></li></a>
+                <a href="#t5"><li class="icon fa fa-code-fork" id="cinco"></li></a>
+              </ul>
+              <div class="page" id="p1">
+                 <section class="icon fa fa-bolt">
+                   <span class="title">Home Section</span>
+                   <span class="hint">This Section Will Explain me<br/>
+                   My BioGraphy My Achivement My Goals My Plans</span>
+                  </section>  
+              </div>
+              <div class="page" id="p2">
+                <section class="icon fa fa-keyboard-o"><span class="title">This Page will Contain all my Stuff realted to designing</span></section>
+              </div> 
+              <div class="page" id="p3">
+                <section class="icon fa fa-rocket"><span class="title">This Page will contain all my Stuff Related to Coding</span></section>
+              </div>
+              <div class="page" id="p4">
+                <section class="icon fa fa-dribbble">
+                  <span class="title">My Stack of Work</span>
+                  <p class="hint">
+                    <a href="https://dribbble.com/albertohartzet" target="_blank">Im ready to play, <span class="hint line-trough">invite me </span> find me</a>
+                  </p>
+                  <p class="hint">Already invited by <a href="http://www.dribbble.com/mrpeters" target="_blank">Stan Peters</a></p>
+                </section>
+              </div> 
+              <div class="page" id="p5">
+                <section class="icon fa fa-plus-circle">
+                  <span class="title">My Repositories and There Descriptions</span>
+                  <p class="hint">
+                    <span>You love one page & CSS only stuff? </span><br/>
+                    <a href="https://codepen.io/hrtzt/details/pgXMYb/" target="_blank">check this pen "Pure CSS One page vertical navigation"</a>
+                  </p>
+                </section>
+                </div> 
+              </div> 
+            </div>
+          </div>
+        </div>
+=======
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+         
+          <h1 className="App-title">Welcome to my profile</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        will start working on my profile development using <react className="js"></react>
         </p>
+>>>>>>> 91ee713e70762649337188c9f424c312afc27be5
       </div>
     );
   }
